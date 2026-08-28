8d2d8e2ceeeb5fc480a35d8a8d6b2c880044711a
Joshua Wilson <jgwilson@nmsu.edu>
2026-08-28 12:52:02 -0600

    Initial commit for Assignment 1

---
+++ assignment1/setup_script.py
@@ -0,0 +1,17 @@
+# %%
+import platform
+
+print(f"I am running on Google's servers: {platform.node()}")
+
+# %% [markdown]
+# #local test Success
+# 
+
+# %%
+name = input("Enter your name: ")
+print(f"Happy coding, {name}!")
+
+# %%
+
+
+
